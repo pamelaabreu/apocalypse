a22db44a00beae496a508b25b23ebf1b1ff69c8a
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-10-14 23:47:00 -0400

    App Skeleton Structure (#2)
    * Added styles to all elements
    
    * Created skeleton JSX for game
    
    * Imported useState functionality from react
    
    * Created state values for game
    
    * Placed state values within JSX elements
    
    * Updated keyboardLetters state value to correctly render letters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,85 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // const [secretWord] = useState();
+  const [guessWord] = useState("_ _ _ _ _");
+  const [numOfGuesses] = useState(6);
+  const [keyboardLetters] = useState({
+    a: { incorrectLetter: false },
+    b: { incorrectLetter: false },
+    c: { incorrectLetter: false },
+    d: { incorrectLetter: false },
+    e: { incorrectLetter: false },
+    f: { incorrectLetter: false },
+    g: { incorrectLetter: false },
+    h: { incorrectLetter: false },
+    i: { incorrectLetter: false },
+    j: { incorrectLetter: false },
+    k: { incorrectLetter: false },
+    l: { incorrectLetter: false },
+    m: { incorrectLetter: false },
+    n: { incorrectLetter: false },
+    o: { incorrectLetter: false },
+    p: { incorrectLetter: false },
+    q: { incorrectLetter: false },
+    r: { incorrectLetter: false },
+    s: { incorrectLetter: false },
+    t: { incorrectLetter: false },
+    u: { incorrectLetter: false },
+    v: { incorrectLetter: false },
+    w: { incorrectLetter: false },
+    x: { incorrectLetter: false },
+    y: { incorrectLetter: false },
+    z: { incorrectLetter: false }
+  });
+  // Extra feature ~ track cpu and user scores
+  // const [cpuScore] = useState();
+  // const [userScore] = useState();
+
+  const keyboardLetterRender = () => {
+    const keyboardLettersKeys = Object.keys(keyboardLetters);
+
+    return keyboardLettersKeys.map((el, index) => (
+      <p key={index}>{el.toUpperCase()}</p>
+    ));
+  };
+
   return (
-    <div className="App">
-      <h1>Apocalypse</h1>
+    <div
+      style={{
+        height: "90vh",
+        border: "1px solid black",
+        marginTop: "10px",
+        marginLeft: "25%",
+        marginRight: "25%"
+      }}
+    >
+      <h1 style={{ margin: "20px 25% 0 25%" }}>Apocalypse</h1>
+      <div style={{ margin: "20px 25% 0 25%" }}>
+        <p>Scoreboard</p>
+        <p>{numOfGuesses} Guesses</p>
+      </div>
+      <div style={{ border: "1px solid red", margin: "20px 25% 0 25%" }}>
+        <p>game image rendering goes here</p>
+      </div>
+      <div
+        style={{ border: "1px solid rebeccapurple", margin: "25% 25% 25% 25%" }}
+      >
+        <div>
+          <p style={{ padding: "20px" }}>{guessWord}</p>
+          <div
+            style={{
+              border: "1px solid blue",
+              margin: "20px",
+              padding: "20px"
+            }}
+          >
+            <p>keyboard</p>
+            <div style={{ display: "flex" }}>{keyboardLetterRender()}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
