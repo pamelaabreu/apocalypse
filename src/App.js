97b2ebc94852644b1a1a83cf20d4a1e33c93f895
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-14 23:33:52 -0400

    Placed state values within JSX elements

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   // const [secretWord] = useState();
-  const [guessWord] = useState();
-  const [numOfGuesses] = useState();
+  const [guessWord] = useState("_ _ _ _ _");
+  const [numOfGuesses] = useState(6);
   const [keyboardLetters] = useState();
   // Extra feature ~ track cpu and user scores
   // const [cpuScore] = useState();
@@ -23,7 +23,7 @@ function App() {
       <h1 style={{ margin: "20px 25% 0 25%" }}>Apocalypse</h1>
       <div style={{ margin: "20px 25% 0 25%" }}>
         <p>Scoreboard</p>
-        <p># of Guesses</p>
+        <p>{numOfGuesses} Guesses</p>
       </div>
       <div style={{ border: "1px solid red", margin: "20px 25% 0 25%" }}>
         <p>game image rendering goes here</p>
@@ -32,7 +32,7 @@ function App() {
         style={{ border: "1px solid rebeccapurple", margin: "25% 25% 25% 25%" }}
       >
         <div>
-          <p style={{ padding: "20px" }}>guessing word goes here</p>
+          <p style={{ padding: "20px" }}>{guessWord}</p>
           <div
             style={{
               border: "1px solid blue",
@@ -40,7 +40,7 @@ function App() {
               padding: "20px"
             }}
           >
-            <p>keyboard goes here</p>
+            <p>keyboard {keyboardLetters}</p>
           </div>
         </div>
       </div>
