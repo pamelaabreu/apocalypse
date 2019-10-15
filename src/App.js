e68fa50b35914bfd9b6fd0fcd268ffebb1c07919
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-15 16:18:26 -0400

    Passed down props to Keyboard component

--- src/App.js
+++ src/App.js
@@ -122,34 +122,6 @@ function App() {
     }
   };
 
-  const keyboardLetterRender = () => {
-    const keyboardLettersKeys = Object.keys(keyboardLetters);
-
-    return keyboardLettersKeys.map((letter, index) => {
-      // Grab the incorrectLetter value from keyboard letters object
-      const { incorrectLetter } = keyboardLetters[letter];
-
-      return (
-        <button
-          key={index}
-          disabled={incorrectLetter}
-          onClick={letterInputClick}
-          style={{
-            margin: "5px",
-            padding: "5px",
-            border: "1px solid black",
-            borderRadius: "100%",
-            height: "30px",
-            width: "30px",
-            textAlign: "center"
-          }}
-        >
-          <p>{letter.toLowerCase()}</p>
-        </button>
-      );
-    });
-  };
-
   return (
     <div
       style={{
@@ -188,8 +160,7 @@ function App() {
                 justifyContent: "center"
               }}
             >
-              <Keyboard />
-              {keyboardLetterRender()}
+              <Keyboard keyboardLetters={keyboardLetters} letterInputClick={letterInputClick} keyboardLettersKeys={Object.keys(keyboardLetters)}/>
             </div>
           </div>
         </div>
