672460f61881b15b0fc75a09a851d45ddd02fb1b
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-16 17:04:09 -0400

    Modified JSX to show basic Modal layout

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,30 +2,19 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-const BootstrapModal = props => {
+const BootstrapModal = ({ show, onHide }) => {
   return (
     <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
       centered
+      backdrop="static"
+      size="xl"
+      aria-labelledby="contained-modal-title-vcenter"
+      show={show}
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
       <Modal.Body>
         <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <Button onClick={onHide}>Start New Game</Button>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 };
