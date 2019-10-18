2f71a7a3d84f065eac04bf7fc43b6b3d212a3681
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-18 19:17:50 -0400

    Modified the class styles for the scoreboard layout

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-const Scoreboard = ({cpuScore, userScore}) => {
-    return <div>
-        <p>User: {userScore}</p>
-        <p>Secret Keeper: {cpuScore}</p>
+const Scoreboard = ({ cpuScore, userScore }) => {
+  return (
+    <div className="row whiteColor justify-content-center">
+      <div className="col-3 p-0 border-right border-light d-flex flex-column justify-content-center align-items-center">
+        <p>User</p>
+        <p>{userScore}</p>
+      </div>
+      <div className="col-3 p-0 border-left border-light d-flex flex-column justify-content-center align-items-center">
+        <p>CPU</p>
+        <p>{cpuScore}</p>
+      </div>
     </div>
+  );
 };
 
 export default Scoreboard;
