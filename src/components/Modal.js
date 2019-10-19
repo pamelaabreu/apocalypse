198231de87a2da53512deb1f36f34ec4ceeb0384
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-18 20:43:06 -0400

    Added bootstrap styles to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,14 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Scoreboard from "./Scoreboard";
 
-const StartNewGameModal = ({ show, userWon, onHide, secretWord, cpuScore, userScore }) => {
+const StartNewGameModal = ({
+  show,
+  userWon,
+  onHide,
+  secretWord,
+  cpuScore,
+  userScore
+}) => {
   const winLoseMessage = userWon
     ? "Ya Win!"
     : `Ya Lose! The correct word was ${secretWord}.`;
@@ -15,10 +22,23 @@ const StartNewGameModal = ({ show, userWon, onHide, secretWord, cpuScore, userSc
       aria-labelledby="contained-modal-title-vcenter"
       show={show}
     >
-      <Modal.Body>
-        <h4>{winLoseMessage}</h4>
-        <Scoreboard cpuScore={cpuScore} userScore={userScore}/>
-        <Button onClick={onHide}>Start New Game?</Button>
+      <Modal.Body className="background-6 d-flex flex-column justify-content-center">
+        {/* <-- Win/Lose Message --> */}
+        <div className="d-flex justify-content-center align-items-center">
+          <h4 className="whiteColor game-text-lg m-5">{winLoseMessage}</h4>
+        </div>
+
+        {/* <-- Scoreboard--> */}
+        <div>
+          <Scoreboard cpuScore={cpuScore} userScore={userScore} />
+        </div>
+
+        {/* <-- Start New Game --> */}
+        <div className="d-flex justify-content-center align-items-center m-5">
+          <Button variant="outline-light" onClick={onHide}>
+            Start New Game?
+          </Button>
+        </div>
       </Modal.Body>
     </Modal>
   );
