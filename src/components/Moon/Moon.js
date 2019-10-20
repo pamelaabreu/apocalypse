0cbd11b54321a7b44e9422d7c68be1bf9ee143d5
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-19 23:24:30 -0400

    Conditional render to show the different moon positions

--- src/components/Moon/Moon.js
+++ src/components/Moon/Moon.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Moon.css";
 
-const Moon = ({numOfGuesses}) => {
-    return <div className="moon"></div>
+const Moon = ({ numOfGuesses }) => {
+  return <div className={"moon moon-" + numOfGuesses}></div>;
 };
 
 export default Moon;
