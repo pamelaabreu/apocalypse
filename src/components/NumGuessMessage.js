680b2c5bb3d9189dcd302767230af89c2697fe83
Pamela Abreu <pamelaabreu@pursuit.com>
2019-10-21 23:21:40 -0400

    Created guessMessage object to render the different messages for each guess number

--- src/components/NumGuessMessage.js
+++ src/components/NumGuessMessage.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const NumGuessMessage = ({numOfGuesses}) => {
-    return <h2>hello num guesses component</h2>
+const NumGuessMessage = ({ numOfGuesses }) => {
+  const guessMessages = {
+    6: "You have six guesses before losing.🤞",
+    5: "Ya goochi 💁",
+    4: "No stress boo u got this💅💅",
+    3: "NO CAP 🚫🧢",
+    2: "oh noes 😬",
+    1: "The world's on fire but everything is fine! 💩💩🤡🤡"
+  };
+
+  return <p className="whiteColor game-text">{guessMessages[numOfGuesses]}</p>;
 };
 
 export default NumGuessMessage;
